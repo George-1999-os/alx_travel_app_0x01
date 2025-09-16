040b1339d0b6041c4d6eb6d8430652e9e43212b2
George-1999-os <georgeibuchi44@gmail.com>
2025-09-16 02:43:23 +0100

    Initial commit for alx_travel_app_0x00

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -1,9 +1,5 @@
-
-from django.urls import path
-
-urlpatterns = []
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -20,6 +16,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('listings.urls')),  # include your listings app routes
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
- (Initial commit for alx_travel_app_0x00)
