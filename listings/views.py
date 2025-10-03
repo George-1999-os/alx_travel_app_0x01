e2079fd34ce61259d18a4f5078f1402e3b3b4c2f
George-1999-os <georgeibuchi44@gmail.com>
2025-10-03 09:01:49 +0100

    Add Listing and Booking API endpoints with proper folder structure

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,21 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Listing, Booking
 from .serializers import ListingSerializer, BookingSerializer
 
 class ListingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows listings to be viewed or edited.
+    """
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 class BookingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows bookings to be viewed or edited.
+    """
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
